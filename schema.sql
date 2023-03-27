67213ca24b047b2f21605274057d5cddea6b4fd3
uchexm <109084069+uchexm@users.noreply.github.com>
2023-03-27 17:46:05 +0200

    Add data to tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,18 @@
-/* Database schema to keep the structure of entire database. */
+-- Table: public.animals
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- DROP TABLE IF EXISTS public.animals;
+
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id integer,
+    name text COLLATE pg_catalog."default",
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg character varying COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
